0c1e7108ead038871c5d4c29e169141e5eb38b2f
Murillop25 <jhondoupp2025@gmail.com>
2025-01-27 15:56:29 -0600

    1

--- formCheck.php
+++ formCheck.php
@@ -3,10 +3,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Formulario con Checkbox/title>
+    <title>Document</title>
 </head>
 <body>
-
 <?php
 
 echo "Hola Mundo";
@@ -14,22 +13,23 @@ echo "<br>";
 echo "Como estas";
 echo "<br>";
 
-//   if (isset($_REQUEST['check1'])) {
-//     $suma = $_REQUEST['valor1'] + $_REQUEST['valor2'];
-//     echo "La suma es:" . $suma . "<br>";
-//   }
-//   elseif (isset($_REQUEST['check2'])) {
-//     $resta = $_REQUEST['valor1'] - $_REQUEST['valor2'];
-//     echo "La resta es:" . $resta;
-//   }else {
-//     echo "Por favor, completa todos los campos correctamente.";
-//   }
-//     echo "<br>";
+  if (isset($_REQUEST['check1'])) {
+    $suma = $_REQUEST['valor1'] + $_REQUEST['valor2'];
+    echo "La suma es:" . $suma . "<br>";
+  }
+  if (isset($_REQUEST['check2'])) {
+    $resta = $_REQUEST['valor1'] - $_REQUEST['valor2'];
+    echo "La resta es:" . $resta;
+  }
+  // else {
+  //   echo "Por favor, completa todos los campos correctamente.";
+  // }
+  //   echo "<br>";
   ?>
 
   <script>
 console.log('Hola mundo');
-
+    
   </script>
 </body>
 </html>
